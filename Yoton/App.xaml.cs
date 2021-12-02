4e6e745da850012a85c566ce2da6d29dc73ae197
Voufack Harold <voufackharold@gmail.com>
2021-12-02 15:43:21 +0100

    Add project files.

---
+++ Yoton/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Yoton
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage( new PageAccueil() );
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Yoton/SinscrireGratuit.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Yoton
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SinscrireGratuit : ContentPage
+    {
+        public SinscrireGratuit()
+        {
+            InitializeComponent();
+
+        }
+
+        async void OnAlertYesNoClicked(object sender, EventArgs e)
+        {
+            bool answer = await DisplayAlert("Cette adresse est déjà connectée à un compte", "Souhaitez--vous plutôt vous connecter ?", "Oui", "Non");
+            //Debug.Writeline("Answer: " + answer);
+        }
+    }
+}
+
